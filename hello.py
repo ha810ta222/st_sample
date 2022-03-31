fe044c4169dc7e99e4827c37c5918657eaab8ded
[ha810ta222] <75781690+ha810ta222@users.noreply.github.com>
2022-03-31 19:23:14 +0900

    main

--- hello.py
+++ hello.py
@@ -5,10 +5,10 @@ st.subheader("It's a page.")
 
 genre = st.radio(
     "What is Your gender?",
-    ('men', 'women' 'other'))
+    ('men', 'women', 'other'))
 
 age = st.slider('How old are you?', 0, 130, 18)
 
 name = st.text_input('What is your name', 'Your name')
 
-st.write('Your Name is' , ' ' , name)
+st.write('Your name is' , ' ' , name , ', your gender is', ' ' , genre,  ', your age is' , ' ', age , ', OK?')

c0af8e43d20ae80a8f31e3ad29b81e0308c00377
[ha810ta222] <75781690+ha810ta222@users.noreply.github.com>
2022-03-31 19:20:29 +0900

    main

--- hello.py
+++ hello.py
@@ -7,7 +7,7 @@ genre = st.radio(
     "What is Your gender?",
     ('men', 'women' 'other'))
 
-age = st.slider('How old are you?', 0, 130 18)
+age = st.slider('How old are you?', 0, 130, 18)
 
 name = st.text_input('What is your name', 'Your name')
 

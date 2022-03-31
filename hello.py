b97a8e58f26f50c0eb7800f4e094d9fe182253ba
[ha810ta222] <75781690+ha810ta222@users.noreply.github.com>
2022-03-31 19:45:45 +0900

    main

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.title("Question about you")
-st.header("by ta2")
+st.header("by ???")
 st.subheader("It's a page.")
 
 genre = st.radio(
@@ -12,3 +12,4 @@ age = st.slider('How old are you?', 0, 130, 18)
 name = st.text_input('What is your name', 'Your name')
 
 st.write('Your name is' , ' ' , name , ', your gender is', ' ' , genre,  ', your age is' , ' ', age , ', OK?')
+

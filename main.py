babb8980d7f97068916797e5d9b564e5e64cd447
[ha810ta222] <75781690+ha810ta222@users.noreply.github.com>
2022-04-07 20:06:23 +0900

    main

--- main.py
+++ main.py
@@ -15,3 +15,30 @@ st.text('PyQRCodeNG version')
 qr_url = st.text_input('Enter a string or URL to generate a QR code:', value='https://code2create.club/')
 
 name = st.text_input('Size', 0)
+
+col1, col2 = st.columns(2)
+with col1:
+    qr_version = st.slider('Version (1-10)', 1, 10, value=5)
+    qr_correction = st.select_slider(
+                        "Error Correction Grade: [L, M, Q, H]",
+                        options=['L', 'M', 'Q', 'H'], value='H')
+    qr_scale = st.slider('Scale (4-8 pixels/cell)', 2, 8, value=4)
+    st.text(f'Length: {len(qr_url)}')
+    st.text(f'QR Code Version: {qr_version}')
+    st.text(f'Error Correction Grade: {qr_correction}')
+    st.text(f'Mode: Scale: {qr_scale}')
+
+try:
+    qr = pyqrcode.create(qr_url, error=qr_correction, version=qr_version, mode='binary')
+    qr.png(QR_FILE, scale=qr_scale, module_color=QR_CODE_COLOR, background=BACKGROUND_COLOR, quiet_zone=4)
+    img = Image.open(QR_FILE)
+    with col2:
+        st.image(img)
+except pyqrcode.DataOverflowError:
+    with col2:
+        st.error(f'Character length {len(qr_url)} is too long to fit within the QR Code.')
+        st.error('Error: Data Overflow. Please try again with a larger version number or a lower Error Correction Grade')
+        st.error('エラー：データあふれ　バージョンを上げたり、エラー訂正のグレードを下げてお試しください。')
+except Exception as e:
+    st.write(e)
+
